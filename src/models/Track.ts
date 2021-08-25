93a75d8de30545ebfa43c9029ed0ae8c2cfb1d77
Catalin-Andrei BORA <catalin.bora@gmail.com>
2021-08-25 22:22:06 +0300

    add update tracking DB Schema;

---
+++ src/models/Track.ts
@@ -0,0 +1,15 @@
+import { Schema, Document, model } from 'mongoose';
+
+export interface Track extends Document{
+    symbol: string;
+    lastUpdate: Date;
+}
+
+const TrackSchema = new Schema<Track>({
+    symbol: { type: String, required: true },
+    lastUpdate: Date,
+});
+
+const TrackModel = model<Track>('Tracks', TrackSchema);
+
+module.exports = TrackModel;
